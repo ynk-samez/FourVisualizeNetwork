8782fe4dd6c8bc36c635e42b855396102b35dcc4
ynk-samez <91073921+ynk-samez@users.noreply.github.com>
2022-12-02 15:09:57 +0900

    Add files via upload

--- visualizeNetwork.m
+++ visualizeNetwork.m
@@ -1,8 +1,6 @@
 function visualizeNetwork(Adj)
 
 config;
-%=======以下、基本はいじらない=========
-
 x=1;y=2;
     %ノードの基本座標生成
 xt=2*pi/3;%size(Adj,x);
@@ -11,7 +9,7 @@ for k=1:3%size(Adj,x)
     P(k,:)=[cos(theta),sin(theta)];
     theta=theta+xt;
 end
-P(4,:)=[0,0]
+P(4,:)=[0,0];
 
 for i=1:size(Adj,y)
     for k=1:size(Adj,x)
@@ -37,11 +35,14 @@ G.Edges.Weight=(Widths.*Widths)';
 G.Edges.Weight=8*G.Edges.Weight/max(G.Edges.Weight);
 figure(1)
     h=plot(G,"XData",P(:,x),"YData",P(:,y),"LineWidth",G.Edges.Weight,"EdgeCData",Widths','EdgeLabel',Widths',"MarkerSize",NODE_SIZE,"NodeColor",NODE_COLOR);
+      ax = gca;
+    ax.XTickLabel = cell(size(ax.XTickLabel));
+    ax.YTickLabel = cell(size(ax.YTickLabel));
     h.NodeFontSize = NODE_LABEL_SIZE;
     h.NodeFontName = NODE_LABEL_FONT;
     h.NodeLabelColor=NODE_LABEL_COLOR;
     h.NodeFontAngle='normal';
-
+    h.ArrowSize=ARROW_SIZE;
     h.EdgeFontName=EDGE_LABEL_FONT;
     h.EdgeLabelColor=EDGE_LABEL_COLOR;
     h.EdgeFontSize=EDGE_LABEL_SIZE;
