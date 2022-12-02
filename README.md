24999149f43476474dd211e787f877e0621467e5
ynk-samez <91073921+ynk-samez@users.noreply.github.com>
2022-12-02 14:45:16 +0900

    add

---
+++ README.md
@@ -0,0 +1,20 @@
+# はじめに
+因果的結合を可視化するためのものです。今後改良していきます.
+
+各領野間の因果関係を示した行列Adjを引数として実行します.
+
+# インストール
+**Matlabのコマンドラインで以下を実行してください。**
+
+- リモートにクローンする。
+```
+git clone https://github.com/ynk-samez/visualizeNetwrok.git
+```
+```
+cd visualizeNetwork
+```
+- 正しく表示されるかのテスト
+```
+MainSample
+```
+<img src= "https://user-images.githubusercontent.com/91073921/204333483-c333866b-5bc9-4b65-8fa2-300e3461d6b8.png" width=320px>
---
+++ printGraph.m
@@ -0,0 +1,56 @@
+function printGraph(Adj,Name)
+
+config;
+%=======以下、基本はいじらない=========
+
+x=1;y=2;
+    %ノードの基本座標生成
+xt=2*pi/3;%size(Adj,x);
+theta=pi/2;
+for k=1:3%size(Adj,x)
+    P(k,:)=[cos(theta),sin(theta)];
+    theta=theta+xt;
+end
+P(4,:)=[0,0];
+
+for i=1:size(Adj,y)
+    for k=1:size(Adj,x)
+        if i==k
+            sAdj(i,k)=0;
+        else 
+            sAdj(i,k)=1;
+        end
+    end
+end
+
+k=1;
+for i=1:numel(Adj)
+    if sAdj(i)==1
+    Widths(k)=round(Adj(i),ROUND);
+        k=k+1;
+    end
+end
+
+G = digraph(sAdj);
+
+G.Edges.Weight=(Widths.*Widths)';
+G.Edges.Weight=8*G.Edges.Weight/max(G.Edges.Weight);
+fig=figure('Visible','off');
+    h=plot(G,"XData",P(:,x),"YData",P(:,y),"LineWidth",G.Edges.Weight,"EdgeCData",Widths','EdgeLabel',Widths',"MarkerSize",NODE_SIZE,"NodeColor",NODE_COLOR);
+    h.NodeFontSize = NODE_LABEL_SIZE;
+    h.NodeFontName = NODE_LABEL_FONT;
+    h.NodeLabelColor=NODE_LABEL_COLOR;
+    h.NodeFontAngle='normal';
+
+    h.ArrowSize=ARROW_SIZE;
+    h.EdgeFontName=EDGE_LABEL_FONT;
+    h.EdgeLabelColor=EDGE_LABEL_COLOR;
+    h.EdgeFontSize=EDGE_LABEL_SIZE;
+    h.NodeLabel;
+    labelnode(h,INDEX,NODE_NAMES);
+    
+    colorbar;
+    colormap(COLORMAP);
+    saveas(fig,Name,"png");
+
+end
